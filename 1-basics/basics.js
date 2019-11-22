39fc6ad5a7c3ed00801a226810a435a83bec6b53
Gordon Oluoch <oyollahg@gmail.com>
2019-11-22 13:35:33 +0300

    weird casting rules

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -249,7 +249,8 @@ describe("basics", function() {
             // By observing their casting behavior in the tests below, you can find
             // out the required types.
             // ✏️ ADD CODE HERE ✏️
-
+            const firstVariable = 3;
+            const secondVariable = "1"
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof firstVariable).to.equal("number");
