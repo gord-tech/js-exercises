fd8ed3c4ffcf63d57931711c2a4c6169c38de923
Gordon Oluoch <oyollahg@gmail.com>
2019-11-22 13:24:04 +0300

    object with property

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -137,7 +137,7 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            var object ={someProperty:1}
             ////////////////////////////////////////////////////////////////////
 
             expect(object instanceof Object).to.equal(true);
